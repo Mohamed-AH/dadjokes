9f47fe64f006802d60aa6680fa7310f1666114c1
Mohamed A Hameed <emah84@gmail.com>
2024-07-11 15:38:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function generateDadJoke() {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            'Authorization': `Bearer ${OPENAI_API_KEY}` // OPENAI_API_KEY will be replaced during build
         },
         body: JSON.stringify({
             prompt: "Generate a dad joke:",
